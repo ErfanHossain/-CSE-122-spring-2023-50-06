8e7513868d990b18b2dabc585c59bb41f7592119
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-01-21 19:28:16 +0600

    Add files via upload

---
+++ Lab2/5.62-50-06-code-71A.cpp
@@ -0,0 +1,27 @@
+/*
+codeforce problem:71A
+prmolem name::: Way Too Long Words
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    while (n--)
+    {
+        cin >> s;
+        if (s.length() > 10)
+        {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
