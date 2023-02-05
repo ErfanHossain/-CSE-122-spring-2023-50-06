bdb8052bad6342183faf96f266c50283fd1d6ac2
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-02-05 18:49:38 +0600

    Add files via upload

---
+++ Lab::4/Bit++.cpp
@@ -0,0 +1,22 @@
+/*Problem Namae:: Bit++
+Problem Number:::282A
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i,x=0;
+    cin>>a;
+    string s;
+    for(i=0;i<a;i++)
+    {
+        cin>>s;
+        if(s[1]=='+')
+            x++;
+        else
+            x--;
+    }cout<<x<<endl;
+
+
+    return 0;
+}
