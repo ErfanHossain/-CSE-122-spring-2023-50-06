1249333965c57a6ad441f3c404e27176cf788a1c
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-02-05 18:33:18 +0600

    Add files via upload

---
+++ Lab::4/Domino_piling .cpp
@@ -0,0 +1,13 @@
+/*Problem Namae:: Domino piling
+Problem Number:::50A
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,sum;
+    cin>>m>>n;
+    sum=(m*n)/2;
+    cout<<sum<<endl;
+    return 0;
+}
