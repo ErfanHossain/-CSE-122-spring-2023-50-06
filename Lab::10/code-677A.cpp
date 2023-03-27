843a3126d7a5c585ffcee3e61d62774c6f882b6b
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-03-27 10:01:12 +0600

    Add files via upload

---
+++ Lab::10/code-677A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,i,b,c=0,d=0;
+    cin>>n>>b;
+    int a;
+    for(i=0;i<n;i++)
+    {
+        cin>>a;
+        if(b<a)
+        {
+            c=c+2;
+        }
+        if(b>=a)
+            d=d+1;
+    }
+    cout<<c+d;
+}
